5848b2378b09ff2b02b74744480ec43e9cb6112b
Mike Lang <mikelang3000@gmail.com>
2017-09-01 22:12:13 -0700

    Initial high level logic and audio test

---
+++ main.asm
@@ -0,0 +1,51 @@
+include "ioregs.asm"
+include "hram.asm"
+
+SECTION "Stack", WRAM0
+
+StackBase:
+	ds 128
+Stack:
+
+
+SECTION "Main", ROM0
+
+
+Start::
+	; Disable LCD, audio
+	xor A
+	ld [SoundControl], A
+	ld [LCDControl], A
+
+	; Load stack
+	ld SP, Stack
+
+	; disable all interrupts
+	xor A
+	ld [InterruptsEnabled], A
+
+	; Initialize sound settings
+	call InitSound
+
+	; Initialize tilemap
+;	call LoadTiles
+
+;	call MainLoop
+
+
+MainLoop::
+	; Display the intro screen
+;	call DisplayIntroScreen
+	ld A, %10010011 ; screen on, background map + sprites, unsigned tileset
+	ld [LCDControl], A	
+
+	; Wait for user to start
+;	call WaitForStart
+
+	; Initialize level number and pass control to level loop
+	xor A
+	ld [LevelNumber], A
+;	call LevelLoop
+
+	; Loop back and return to intro screen
+	jp MainLoop
