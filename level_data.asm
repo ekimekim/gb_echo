eac268082e991565dcec3363ff389d99cc9d744b
Mike Lang <mikelang3000@gmail.com>
2017-09-04 13:01:11 -0700

    Create levels
    Broadly:
    	1: simple level for teaching mechanics
    	2. simple level exploring mechanics more with a more complex space
    	3. series of adjoined rooms with light dead ends
    	4. full-on traditional maze. might be too hard?
    	5. finale, a giant hall with pillars and a triple-crystal ending

--- level_data.asm
+++ level_data.asm
@@ -20,30 +20,171 @@ LevelData::
 
 
 L1:
-	db 10, 10, 3, 8
-	db "xxxxxxxxxx"
-	db "x        x"
-	db "x        x"
-	db "x        x"
-	db "x        x"
-	db "x     x  x"
-	db "x     x  x"
-	db "x     x  x"
-	db "x     x* x"
-	db "xxxxxxxxxx"
+	db 13, 15, 3, 13
+	db "xxxxxxxxxxxxx"
+	db "x      x  * x"
+	db "x      x    x"
+	db "x      x    x"
+	db "x      x    x"
+	db "x      x    x"
+	db "xxxxxxxx    x"
+	db "x           x"
+	db "x           x"
+	db "x     xxxxxxx"
+	db "x     x     x"
+	db "x     x     x"
+	db "x     x     x"
+	db "x     x     x"
+	db "xxxxxxxxxxxxx"
 L1e:
 L1SIZE EQU L1e - L1
-	CheckSize L1, L1SIZE, 10, 10
+	CheckSize L1, L1SIZE, 13, 15
 
 
 L2:
-	db 4, 3, 1, 1
-	db "xxxx"
-	db "x *x"
-	db "xxxx"
+	db 11, 13, 1, 1
+	db "xxxxxxxxxxx"
+	db "x x       x"
+	db "x x       x"
+	db "x x       x"
+	db "x x       x"
+	db "x xxxxxxxxx"
+	db "x         x"
+	db "x         x"
+	db "x         x"
+	db "x   xxx   x"
+	db "x   x*x   x"
+	db "x         x"
+	db "xxxxxxxxxxx"
 L2e:
 L2SIZE EQU L2e - L2
-	CheckSize L2, L2SIZE, 4, 3
+	CheckSize L2, L2SIZE, 11, 13
+
+
+L3:
+	db 15, 18, 12, 13
+	db "xxxxxxxxxxxxxxx"
+	db "x     x*      x"
+	db "x     xxxxxxx x"
+	db "x     x       x"
+	db "x             x"
+	db "x     x       x"
+	db "xxxxxxx       x"
+	db "x     x       x"
+	db "x             x"
+	db "x     x       x"
+	db "x     xxxxxxxxx"
+	db "x     x       x"
+	db "x     x       x"
+	db "xxxxx x       x"
+	db "x   x         x"
+	db "x   x x       x"
+	db "x     x       x"
+	db "xxxxxxxxxxxxxxx"
+L3e:
+L3SIZE EQU L3e - L3
+	CheckSize L3, L3SIZE, 15, 18
+
+
+L4:
+	db 31, 31, 1, 1
+	db "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+	db "x           x     x     x     x"
+	db "x           x     x     x     x"
+	db "x  x  xxxx  x  x  x  xxxx  x  x"
+	db "x  x  x     x  x           x  x"
+	db "x  x  x     x  x           x  x"
+	db "x  x  xxxxxxx  xxxx  xxxxxxxxxx"
+	db "x  x        x  x     x        x"
+	db "x  x        x  x     x        x"
+	db "x  xxxxxxx  x  x  xxxx  xxxx  x"
+	db "x  x     x  x  x        x     x"
+	db "x  x     x  x  x        x     x"
+	db "x  x  x  x  x  xxxxxxxxxx  xxxx"
+	db "x     x  x     x     x     x  x"
+	db "x     x  x     x     x     x  x"
+	db "xxxxxxx  xxxxxxx  xxxx  xxxx  x"
+	db "x  x     x  x           x     x"
+	db "x  x     x  x           x     x"
+	db "x  x  xxxx  x  xxxxxxxxxxxxx  x"
+	db "x  x     x  x  x              x"
+	db "x  x     x  x  x              x"
+	db "x  xxxx  x  x  x  xxxxxxxxxx  x"
+	db "x           x     x     x     x"
+	db "x           x     x     x     x"
+	db "xxxx  xxxxxxxxxxxxx  x  xxxx  x"
+	db "x     x        x     x  x     x"
+	db "x     x        x     x  x     x"
+	db "x  xxxx  xxxx  x  xxxx  x  x  x"
+	db "x        x     x     x     x  x"
+	db "x        x     x     x     x *x"
+	db "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+L4e:
+L4SIZE EQU L4e - L4
+	CheckSize L4, L4SIZE, 31, 31
+
+
+L5:
+	db 41, 56, 20, 54
+	db "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+	db "x                                       x"
+	db "x                                       x"
+	db "x                                       x"
+	db "x                                       x"
+	db "x               xxxxxxxxx               x"
+	db "x               xx     xx               x"
+	db "x               x  x x  x               x"
+	db "x               x x* *x x               x"
+	db "x               x  x*x  x               x"
+	db "x               xx xxx xx               x"
+	db "x                x     x                x"
+	db "x            xxx         xxx            x"
+	db "x            xxx         xxx            x"
+	db "x            xxx         xxx            x"
+	db "x                                       x"
+	db "x            xxx         xxx            x"
+	db "x            xxx         xxx            x"
+	db "x            xxx         xxx            x"
+	db "x                                       x"
+	db "x            xxx         xxx            x"
+	db "x            xxx         xxx            x"
+	db "x            xxx         xxx            x"
+	db "x                                       x"
+	db "x                                       x"
+	db "x                                       x"
+	db "x                                       x"
+	db "x          xxx             xxx          x"
+	db "x          xxx             xxx          x"
+	db "x          xxx             xxx          x"
+	db "x                                       x"
+	db "x                                       x"
+	db "x                                       x"
+	db "x                                       x"
+	db "x        xxx                 xxx        x"
+	db "x        xxx                 xxx        x"
+	db "x        xxx                 xxx        x"
+	db "x                                       x"
+	db "x                                       x"
+	db "x                                       x"
+	db "x                                       x"
+	db "x      xxx                     xxx      x"
+	db "x      xxx                     xxx      x"
+	db "x      xxx                     xxx      x"
+	db "x                                       x"
+	db "x                                       x"
+	db "x                                       x"
+	db "x                                       x"
+	db "x    xxx                         xxx    x"
+	db "x    xxx                         xxx    x"
+	db "x    xxx                         xxx    x"
+	db "x                                       x"
+	db "x                                       x"
+	db "x                                       x"
+	db "x                                       x"
+	db "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+L5e:
+L5SIZE EQU L5e - L5
+	CheckSize L5, L5SIZE, 41, 56
 
 
 LevelDataEnd::
